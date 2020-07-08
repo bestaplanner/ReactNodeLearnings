c7c7b1b9e1f09020cbc4c98f5b97081d141b5a97
Nishanth Sreenivasa <nishanth.srinivasa@capgemini.com>
2020-07-08 10:40:56 +0530

    initial setup

---
+++ my-app/json-server/public/precache-manifest.58231c3485ae7f3c7c712545a0851e05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7bdbed70d7501d125acee2626905a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a9da76a0bf21ee1d2fb",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "f52e6d2ada346320d16f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2a9da76a0bf21ee1d2fb",
+    "url": "/static/js/2.71fc65ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.71fc65ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52e6d2ada346320d16f",
+    "url": "/static/js/main.2ba85ebb.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
